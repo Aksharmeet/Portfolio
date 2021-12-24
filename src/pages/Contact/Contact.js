431b99e1fb68a6fa6dc03ba6dc84ab15918827bf
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2021-12-24 15:22:51 +0530

    Form on Contact page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,111 @@
+import React, {useState} from 'react'
+import Header from '../../Common Components/Header'
+import Footer from '../../Common Components/Footer'
+import styled from 'styled-components'
+
+// Importing formInputs
+import FormInput from './FormInput'
+function About() {
+    const [values, setValues] = useState({
+        username:"",
+        email: "",
+        message: "",
+    })
+    const Inputs = [
+        {
+            id:1,
+            name:"username",
+            type:"text",
+            placeholder:"Your name??",
+            errorMessage:"Name should be 3-16 characters and shouldn't include any special characters(example:!,@,#,$) !",
+            label:"Username",
+            pattern:"^[A-Za-z0-9]{3,16}$",
+            required: true,
+
+        },
+        {
+            id:2,
+            name:"email",
+            type:"email",
+            placeholder:"Your e-mail",
+            errorMessage:"Enter a valid e-mail address",
+            label:"E-mail",
+            required: true,
+
+        },
+        {
+            id:3,
+            name:"message",
+            type:"text",
+            placeholder:"Here you go...",
+            errorMessage:"Message must be of atleast 50 characters and must not include any special characters",
+            label:"Message",
+            required: true,
+        }
+    ]
+    const [SubmitState, setSubmitState] = useState(false)
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        if(e.target.checkValidity()){
+            const Submitted = document.getElementById('submitted');
+           
+        }
+    }
+    const onChange = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value})
+    }
+    return (
+        <div>
+           <Header/>
+            <Intro>
+                <h4>
+                   Want to Develop a website? 
+                   Looking for a react developer?
+                   Let's find out how i can help you !  
+                </h4>
+                
+            </Intro>
+            <FormActual onSubmit={handleSubmit}>
+            {Inputs.map((input) => (
+                <FormInput key={input.id} {...input} onChange={onChange} value={values[input.name]}  />
+            ))}
+            <button>Submit</button>
+            </FormActual>
+            <Footer/>
+        </div>
+    )
+}
+const Intro = styled.div`
+    font-family:raleway;
+    font-size:2rem;
+    margin:50px auto;
+    padding: 0 40px;
+    @media (min-width:760px){
+        font-size:3rem;
+    }
+    @media(min-width:1100px){
+        font-size:4rem;
+    }
+    
+`
+const FormActual = styled.form`
+    padding:0 40px;
+    
+button{
+    width:100%;
+    font-family:raleway;
+    font-size:1.5rem;
+    font-weight:500;
+    padding:20px 0px;
+     background-color:rgb(32,28,68);
+     color:#fff;
+     border-style:none;
+     border:0px solid #000;
+     margin:60px auto;
+     border-radius:5px;
+    cursor:pointer;
+}
+
+`
+
+export default About
---
+++ src/pages/Contact/FormInput.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+function FormInput(props) {
+    const {label, errorMessage, onChange, ...inputProps}= props;
+    const [Focused, SetFocus] = useState(false);
+    const handleFocus = () =>{
+        SetFocus(true);
+    }
+    
+    return (
+        <div>
+       <Form Focused={Focused}>
+           <label>{label}</label>
+           <input  {...inputProps} onChange={onChange} onBlur={handleFocus}   Focused={Focused.toString()} > 
+           </input>
+           <span><p>{errorMessage}</p></span>
+       </Form>
+       <Submitted>
+
+       </Submitted>
+       </div>
+    )
+}
+
+const Form = styled.div`
+    margin-top:50px;
+    display:flex;
+    flex-direction:column;
+    font-family:raleway;
+    font-size:1.5rem;
+    font-weight:500;
+   
+    input, textarea, button{
+        font-size:1.3rem;
+        padding:15px 10px;
+        border-radius:5px;
+        border-style:none;
+        border:3px solid #000;
+
+    }
+    span{
+        display:none;
+    }
+    input{
+        margin:10px 0 0 0;
+        
+    }
+    input:invalid[Focused="true"] {
+        border: 3px solid red;
+    }
+    input:invalid[Focused="true"] ~ span{
+        display:block;
+    }
+    textarea{
+        margin:10px 0 30px 0;
+    
+    }
+   
+  
+    p{
+        margin:8px 0 0px 0;
+        font-weight:400;
+        font-size:1.2rem;
+        color:red;
+       
+
+    }
+
+`
+const Submitted = styled.div`
+`
+
+
+export default FormInput
