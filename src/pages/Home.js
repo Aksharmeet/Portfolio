b1f37b6f025ffc368640811bb552967b37943c7f
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2022-12-13 18:15:56 +0530

    rand

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,12 @@ function Home() {
 					<a href='mailto:aksharmeetsingh21@gmail.com'>
 						<p className='aksharmeet'>
 							aksharmeetsingh21@gmail.com
-							<img src='https://uploads-ssl.webflow.com/5d0f20ee6c004646f3c81f02/615bf8db0a1498233ce772d6_arrow-01.svg' alt='mail arrow' />
+							<img
+								src='https://uploads-ssl.webflow.com/5d0f20ee6c004646f3c81f02/615bf8db0a1498233ce772d6_arrow-01.svg'
+								alt='mail arrow'
+								width='590'
+								height='335'
+							/>
 						</p>
 					</a>
 				</div>
@@ -23,7 +28,7 @@ function Home() {
 					<div className='bg-red'>
 						<a href='https://sanity-blog-vercel.vercel.app/' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop position1' src='./images/SanityBlog.png' alt='sanity blog' />
+								<img className='tesla-desktop position1' src='./images/SanityBlog.png' alt='sanity blog' width='590' height='335' />
 							</ImageWrapper>
 							<p>THE BLOG</p>
 						</a>
@@ -34,7 +39,7 @@ function Home() {
 					<div className='bg-orange'>
 						<a href='https://hopeful-murdock-c41964.netlify.app' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop position1' src='./images/Resturant-d.jpg' alt='resturant' />
+								<img className='tesla-desktop position1' src='./images/Resturant-d.jpg' alt='resturant' width='590' height='335' />
 							</ImageWrapper>
 							<p>The Returant</p>
 						</a>
@@ -47,7 +52,7 @@ function Home() {
 					<div className='bg-red'>
 						<a href='https://netflix-63936.firebaseapp.com' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop position1' src='./images/Netflix-d.jpg' alt='netflix' />
+								<img className='tesla-desktop position1' src='./images/Netflix-d.jpg' alt='netflix' width='590' height='335' />
 							</ImageWrapper>
 							<p>Netflix-clone</p>
 						</a>
@@ -58,7 +63,7 @@ function Home() {
 					<div className='bg-red'>
 						<a href='https://joyful-trifle-6e97d3.netlify.app/' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop position1' src='./images/mini-task.png' alt='task' />
+								<img className='tesla-desktop position1' src='./images/mini-task.png' alt='task' width='590' height='335' />
 							</ImageWrapper>
 							<p>Mini Task Manager</p>
 						</a>
@@ -70,7 +75,7 @@ function Home() {
 					<div className='bg-blue'>
 						<a href='https://goofy-borg-ab2033.netlify.app' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop' src='./images/Tesla-d.jpg' alt='tesla' />
+								<img className='tesla-desktop' src='./images/Tesla-d.jpg' alt='tesla' width='590' height='335' />
 							</ImageWrapper>
 							<p>Tesla Website Clone</p>
 						</a>
@@ -80,7 +85,7 @@ function Home() {
 					<div className='bg-blue'>
 						<a href='https://kaleidoscopic-puffpuff-9b5100.netlify.app/' target='_blank' rel='noreferrer'>
 							<ImageWrapper>
-								<img className='tesla-desktop' src='./images/LazyFetch.png' alt='lazy fetch' />
+								<img className='tesla-desktop' src='./images/LazyFetch.png' alt='lazy fetch' width='590' height='335' />
 							</ImageWrapper>
 							<p>Lazy Fetch</p>
 						</a>
